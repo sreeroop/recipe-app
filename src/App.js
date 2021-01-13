fdd7ab8a8cf2afe500894627e66d9562946af3ca
sreeroop <sreeroopsk10@gmail.com>
2021-01-13 17:01:04 +0530

    uploading github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import Recipe from './Recipe';
+import "./App.css";
 
-function App() {
+const App = () => {
+  const APP_ID = "dcc7311e";
+  const APP_KEY = "09964f511ed76f021e7d50013e804f9e";
+
+  const [recipies, setRecipies] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState('chicken')
+   
+  useEffect(() => {
+    getRecipies();
+  }, [query]);
+
+  const getRecipies = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    setRecipies(data.hits);
+  };
+
+  const updateSearch = e => {
+    setSearch(e.target.value);
+  };
+
+  const getsearch = e => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch("");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="search-form" onSubmit={getsearch} >
+        <input className="search-bar" type="text" value={search} onChange={updateSearch} />
+        <button className="search-button" type="submit" >
+          search
+        </button>
+      </form>
+      <div className="recipies">
+        {recipies.map(recipe => (
+        <Recipe
+          key={recipe.recipe.label}
+          title={recipe.recipe.label}
+          calories={recipe.recipe.calories}
+          image={recipe.recipe.image}
+          ingredients={recipe.recipe.ingredients} />
+      ))}
+      </div>
+      
     </div>
   );
-}
+};
 
 export default App;
